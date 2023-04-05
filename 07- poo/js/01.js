7a705846341bb9ec61a9293eefc25fed405ff58f
victoralmeida20 <victorxf10@yahoo.com>
2023-04-04 21:15:18 -0300

    Exercicios

---
+++ 07- poo/js/01.js
@@ -0,0 +1,15 @@
+let Carro ={
+    marca:"Renalt",
+    modelo:"Sandero",
+    ano : 2022
+}
+console.log(Carro.marca);
+
+let Pessoa ={
+    nome:"Carlos",
+    idade: 0,
+    peso: 90,
+    casado: true
+}
+console.log("Nome: " + Pessoa.nome);
+console.log("idade:" + Pessoa.idade);
---
+++ 07- poo/js/02.js
@@ -0,0 +1,6 @@
+let Conta ={
+    titular:"Carlos",
+    saldo:5200,
+    
+}
+console.log("Titular:"+ Conta.titular + " - Saldo: "+Conta.saldo);
